2b2e4d826e3abba94ffb42a7d5cd886460c37561
fabian-079 <fabian.alejandro077@gmail.com>
2026-06-17 01:46:55 -0500

    Corregir persistencia del worker en entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# 1. Recolectar estáticos (aquí sí tenemos acceso a las variables de entorno)
+
+# 1. Recolectar estáticos
 python manage.py collectstatic --noinput
 
 # 2. Aplicar migraciones
 python manage.py migrate --noinput
 
 # 3. Iniciar el procesador de tareas
-python manage.py process_tasks &
+# --duration=0: hace que el worker no se detenga después de procesar una tarea
+# --queue: asegura que atienda la cola por defecto de forma continua
+python manage.py process_tasks --duration=0 --queue &
 
 # 4. Iniciar Gunicorn
 exec gunicorn wsgi:application --bind 0.0.0.0:8000
